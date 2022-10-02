9925db694de6828d24b8d536b1f8949864eeb0fb
Khaled Alghanim <al.ghanim@me.com>
2022-10-02 11:35:33 +0300

    Task is solved!

--- age_calculator.py
+++ age_calculator.py
@@ -1,19 +1,33 @@
+from ast import Try
+from calendar import month
 from datetime import date
 
 
 def get_dob():
     # write code here
-	...
-
-
+	year = int(input("Enter year of birth: "))
+	month = int(input("Enter month of birth: "))
+	day = int(input("Enter day of birth: "))
+	
+	return date(year,month,day)
+	
 def get_age(dob):
     # write code here
-	...
 
+	today = date.today()
+	age = int(today.year - dob.year)
+
+	return age
 
 def main():
 	# write code here
-	...
+	
+	dob = get_dob()
+	if dob.year > date.today().year :
+		print ("Invalid date of birth!")
+	else:
+		age = get_age(dob)
+		print(f"You are {age} years old.")
 
 
 if __name__ == '__main__':
